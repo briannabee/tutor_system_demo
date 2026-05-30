488040e5ef0e0e77453fd3144dd197511ecf61b0
BRIANNA DYSINGER <BDYSINGER1@UCO.EDU>
2026-05-30 14:33:05 -0500

    first commit

---
+++ public/controller/app.js
@@ -0,0 +1,99 @@
+import { ProfileView } from "../view/ProfileView.js";
+import { HomeView } from "../view/HomeView.js";
+import { HomeController } from "./HomeController.js";
+import { ProfileController } from "./ProfileController.js";
+import { Router } from "./Router.js";
+import { loginFirebase, logoutFirebase, createAccount } from "./firebase_auth.js";
+import { startSpinner, stopSpinner } from "../view/util.js";
+
+document.getElementById('appHeader').textContent = 'Cloud Web Template';
+document.title = 'App Template';
+
+const routes = [
+    {path: '/', view: HomeView, controller: HomeController},
+    {path: '/profile', view: ProfileView, controller: ProfileController}
+];
+
+//instance of router
+export const router = new Router(routes);
+router.navigate(window.location.pathname);
+
+const menuItems = document.querySelectorAll('a[data-path]');
+menuItems.forEach(item => {
+    item.onclick = function(e) {
+        const path = item.getAttribute('data-path');
+        router.navigate(path);
+    }
+});
+
+//login form
+document.forms.loginForm.onsubmit = async function(e) {
+    e.preventDefault(); //prevent from page reload
+    const email = e.target.email.value;
+    const password = e.target.password.value;
+    startSpinner();
+    try {
+        await loginFirebase(email, password);
+        stopSpinner();
+        console.log('User logged in', email);
+    } catch(e) {
+        stopSpinner();
+        console.error('Error logging in:', e);
+        const errorCode = e.code;
+        const errorMessage = e.message;
+        alert('Sign in failed: ' + error.code + ', ' + errorMessage);
+    }
+};
+
+//logout button
+document.getElementById('logoutButton').onclick = async function(e) {
+    startSpinner();
+    try {
+        await logoutFirebase();
+        stopSpinner();
+        console.log('User logged out');
+    } catch(e) {
+        stopSpinner();
+        console.error('Error logging out:', e);
+        const errorCode = e.code;
+        const errorMessage = e.message;
+        alert('Sign out failed: ' + error.code + ', ' + errorMessage);
+    }
+};
+
+//create account form
+document.forms.createAccountForm.onsubmit = async function(e) {
+    e.preventDefault(); //prevent from page reload
+    const email = e.target.email.value;
+    const emailConfirm = e.target.emailConfirm.value;
+    if (email != emailConfirm) {
+        alert('Emails do not match');
+        return;
+    }
+    const password = e.target.password.value;
+    startSpinner();
+    try {
+        await createAccount(email, password);
+        stopSpinner();
+        document.getElementById('createAccountDiv').classList.replace('d-block', 'd-none');
+    } catch(e) {
+        stopSpinner();
+        console.error('Error creating user:', e);
+        const errorCode = e.code;
+        const errorMessage = e.message;
+        alert('Create account failed: ' + e.code + ', ' + e.Message);
+    }
+}
+
+// show create account form / hide login form
+document.getElementById('goToCreateAccount').onclick = function(e) {
+    document.getElementById('loginDiv').classList.replace('d-block', 'd-none');
+    document.getElementById('createAccountDiv').classList.replace('d-none', 'd-block');
+    document.forms.createAccountForm.reset();
+};
+
+//vice versa
+document.getElementById('goToLogin').onclick = function(e) {
+    document.getElementById('createAccountDiv').classList.replace('d-block', 'd-none');
+    document.getElementById('loginDiv').classList.replace('d-none', 'd-block');
+};
---
+++ public/controller/firebase_core.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.2.0/firebase-app.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyDsfKQ9wYAwuLPVr1p51vEI-V0kLInVzfQ",
+    authDomain: "bdysinger1-cmsc4373.firebaseapp.com",
+    projectId: "bdysinger1-cmsc4373",
+    storageBucket: "bdysinger1-cmsc4373.firebasestorage.app",
+    messagingSenderId: "124055204345",
+    appId: "1:124055204345:web:0653488b3fff5468ea0d28"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
